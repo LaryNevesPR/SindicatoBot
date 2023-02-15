b7318abdc9082b3a5ca0cdbb5a5da66298a00e9a
Thales Neves <thalesnevespr@gmail.com>
2023-02-15 03:28:18 -0300

    Mensagem Teste

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def on_member_join(member):
     guild = member.guild
     await channel.edit(name=f"Membros: {guild.member_count}")
 
-@bot.slash_command(name="senha")
+@bot.slash_command(name="senha", description= "Senha necessária para se tornar um membro.")
 async def senha(ctx, senha:Option(str, "Digite a senha!", required = True)):
     senha = senha.lower()
     print(senha)
@@ -35,4 +35,17 @@ async def senha(ctx, senha:Option(str, "Digite a senha!", required = True)):
     
     await ctx.respond(f'{ctx.author}', ephemeral= True)
 
+@bot.slash_command(name = "inicio")
+async def inicio(ctx):
+    await ctx.send(file=discord.File("Recursos/Teste.png"))
+    
+    embed = discord.Embed(title = "【Ｂｅｍ　ｖｉｎｄｏ　ａｏ　Ｓｉｎｄｉｃａｔｏ！】", description ="Frase motivacional 1", color = discord.Color.gold())
+    embed.add_field(name="Titulo 1", value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pretium sapien quis volutpat blandit. Nullam eu mattis urna. ")
+    embed.add_field(name="Titulo 3", value="Quisque ac velit vitae massa suscipit condimentum. In vitae mattis libero. Donec blandit mi nec ornare fermentum.")
+    embed.set_footer(text="Footer", icon_url= ctx.user.avatar)
+    embed.set_image(url="https://i.imgur.com/5ZO3qC9.png")
+    #embed.set_author(name=ctx.author.name, icon_url=ctx.user.avatar)
+    await ctx.send(embed=embed)
+
+    
 bot.run(Key)
